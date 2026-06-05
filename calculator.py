04d80b442056c3e07e047b85e9095b4d115f130f
Prithika S J <prithika161106@gmail.com>
2026-06-05 23:56:21 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1=float(input("Enter first number:"))
+num2=float(input("Entrer second number:"))
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+if num2!=0:
+    print("Divison:",num1/num2)
+else:
+    print("Cannot divide by zero")
